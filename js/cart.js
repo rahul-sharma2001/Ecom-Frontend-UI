38ca7e611d2667d63437c1a169bc4a1cfdbf27a0
Vraj Panchal <vraj@cloudmatetechnologies.com>
2023-01-30 16:00:41 +0530

    refactor : css naming-convention

--- js/cart.js
+++ js/cart.js
@@ -1,30 +1,3 @@
-function getSessionStorageDataByKey(key, defaultValue = undefined) {
-    let data = sessionStorage.getItem(key);
-    if (data) {
-        return typeof data == "string" ? data : JSON.parse(data)
-    }
-    else {
-        return defaultValue;
-    }
-}
-
-let isSignUp = getSessionStorageDataByKey("isSignUp", true)
-let isOutOfCart = getSessionStorageDataByKey("isOutOfCart", false);
-let isAddressSelected = getSessionStorageDataByKey("isAddressSelected", false)
-let isPaymentDone = getSessionStorageDataByKey("isPaymentDone", false)
-function setHeaderTagIcons(isOutOfCart, isAddressSelected, isPaymentDone) {
-    const cartTag = document.getElementById("header-cartTag")
-    const addressTag = document.getElementById("header-addressTag")
-    const paymentTag = document.getElementById("header-paymentTag")
-    cartTag.innerHTML = `<img class="h-3vw mr-2 py-1" src="../images/${!isOutOfCart ? "edit-icon.svg" : "tick-icon.svg"}" alt="1" srcset="">
-                            CART`
-    addressTag.innerHTML = ` <img class="h-3vw mr-2 py-1" src="../images/${(!isOutOfCart && !isAddressSelected) ? "2.png" : (isOutOfCart && !isAddressSelected) ? "edit-icon.svg" : isOutOfCart ? "tick-icon.svg" : "2.png"}" alt="2" srcset="">
-                            ADDRESS`
-    paymentTag.innerHTML = ` <img class="h-3vw mr-2 py-1" src="../images/${(isOutOfCart && !isAddressSelected && !isPaymentDone) ? "3.png" : (isOutOfCart && isAddressSelected && !isPaymentDone) ? "edit-icon.svg" : (isOutOfCart && isAddressSelected) ? "tick-icon.svg" : "3.png"}" alt="3" srcset=""> PAYMENT`
-}
-setHeaderTagIcons(isOutOfCart, isAddressSelected, isPaymentDone)
- 
-
 let cartProductArr = [
     {
         "_id": {
@@ -172,9 +145,7 @@ let cartProductArr = [
       }
 ]
 
-
-
-let productsDetailsGroup = document.querySelector(".product-details-group")
+let productsDetailsGroup = document.querySelector(".top-div__product-details__wrapper")
 
 let productInCart = cartProductArr
 console.log("ProductInCart", productInCart)
@@ -186,58 +157,60 @@ productInCart.map((element, index) => {
     // productInCart.push(element)
     productsDetailsGroup.innerHTML+=
     `
-                        <div class="product-detail d-flex border p-3 mb-3 bg-white rounded  border-shadow">
-                            <div class="porduct-img-div mr-3" >
-                                <img class="product-img"  width="120px" src="${element.imageUrl}" alt="">
-                               
+                        <div class="card-product-details d-flex border p-3 mb-3 bg-white rounded">
+                            <div class="card-product-details__figure mr-3" >
+                                <img class="card-product-details__img"  width="120px" src="${element.imageUrl}" alt="">
                             </div>
-                            <div>
-                                <div class="product-name fs-6 font-weight-bold">${element.brandName}</div>
-                                <div class="product-color fs-6 text-secondary">${element.materialDetails}</div>
-                                <div class="product-manufacture fs-7 text-muted">${element.category}</div>
-                                <div class="my-2 card-btn-group">
-                                    <div class="dropdown display-inline-block ">
-                                        <button class="product-quantity-dd btn btn-sm dropdown-toggle bg-white border-primary-color font-weight-bold mb-2" type="button" id="dropdownMenuButton"
+                            <div class="card-product-details__body">
+                                <div class="card-product-details__name fs-6 font-weight-bold">${element.brandName}</div>
+                                <div class="card-product-details__color fs-6 text-secondary">${element.materialDetails}</div>
+                                <div class="card-product-details__manufacturer fs-7 text-muted">${element.category}</div>
+                                <div class="card-product-details__btn-wrapper my-2 ">
+                                    <div class="dropdown  ">
+                                        <button class="dropdown-btn btn btn-sm dropdown-toggle  font-weight-bold mb-2 bg-white" type="button" id="dropdownMenuButton"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Size
+                                         Size
                                         </button>
-                                        <div class="dropdown-menu border-primary-color" aria-labelledby="dropdownMenuButton">
-                                          <a class="dropdown-item text-primary-color font-weight-bold " href="#">${element.sizes[0]}</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">${element.sizes[1]}</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">${element.sizes[2]}</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">${element.sizes[3]}</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">${element.sizes[4]}</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">${element.sizes[5]}</a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                          <a class="dropdown-item font-weight-bold " href="#">${element.sizes[0]}</a>
+                                          <a class="dropdown-item font-weight-bold" href="#">${element.sizes[1]}</a>
+                                          <a class="dropdown-item font-weight-bold" href="#">${element.sizes[2]}</a>
+                                          <a class="dropdown-item font-weight-bold" href="#">${element.sizes[3]}</a>
+                                          <a class="dropdown-item font-weight-bold" href="#">${element.sizes[4]}</a>
+                                          <a class="dropdown-item font-weight-bold" href="#">${element.sizes[5]}</a>
                                         </div>
                                     </div>
-                                    <div class="dropdown display-inline-block ">
-                                        <button class="product-quantity-dd btn btn-sm dropdown-toggle bg-white border-primary-color font-weight-bold mb-2" type="button" id="dropdownMenuButton"
+                                    <div class="dropdown ">
+                                         <button class="dropdown-btn btn btn-sm dropdown-toggle font-weight-bold mb-2 bg-white" type="button" id="dropdownMenuButton"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                          Qty:1
                                         </button>
-                                        <div class="dropdown-menu border-primary-color  " aria-labelledby="dropdownMenuButton">
-                                          <a class="dropdown-item text-primary-color font-weight-bold  "href="#">2</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">3</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">4</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">5</a>
-                                          <a class="dropdown-item text-primary-color font-weight-bold" href="#">6</a>
+                                        <div class="dropdown-menu border-primary-color" aria-labelledby="dropdownMenuButton">
+                                         <a class="dropdown-item  font-weight-bold  "href="#">2</a>
+                                          <a class="dropdown-item  font-weight-bold" href="#">3</a>
+                                          <a class="dropdown-item  font-weight-bold" href="#">4</a>
+                                          <a class="dropdown-item  font-weight-bold" href="#">5</a>
+                                          <a class="dropdown-item  font-weight-bold" href="#">6</a>
                                         </div>
                                     </div>
 
                                 </div>
-                                <div class="product-price-div fs-6 mb-2">
+                                <div class="card-product-details__price-details fs-6 mb-2">
                                     <span class="font-weight-bold"> ₹ ${element.price} </span>
-                                    <span class="mx-1 text-decoration-line-through text-secondary">₹ 4,499</span>
-                                    <span class="highlight-color font-weight-bold">${element.discount}% OFF</span>
-                                </div>
+                                    <span class="card-product-discount mx-1 ">₹ 4,499</span>
+                                    <span class="card-product-details__discount-per font-weight-bold">${element.discount}% OFF</span>
+                                </div> 
                                 <div class="fs-6 mb-3"> 
                                     <span><img src="../images/exchange.png" width="20px" alt=""></span> 
                                     <strong>30 days</strong> return available 
                                 </div>
                                 <div class="card-btn-group " >
-                                    <button type="button" class="product-delete-btn btn btn-sm btn-danger font-weight-bold mb-2" onclick="deleteProduct(${index})">Delete</button>
-                                    <button type="button" class=" btn btn-sm text-secondary-color bg-white border-secondary-color rounded font-weight-bold mb-2">
-                                    <i class="fa fa-heart-o " id="heart"></i>
+                                    <button type="button" class="
+                                    product-delete-btn btn btn-sm btn-danger font-weight-bold mb-2" onclick="deleteProduct(${index})">Delete</button>
+
+                                    <button type="button" class="
+                                    product-wishlist-btn btn btn-sm bg-white rounded font-weight-bold mb-2">
+                                    <i class="wishlist-icon fa fa-heart-o"></i>
                                     <span class="fs-7 font-weight-bold">WISHLIST</span></button>
                                 </div>
                             </div>
