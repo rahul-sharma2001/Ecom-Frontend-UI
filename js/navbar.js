5392103e3cba1638943ddd99f298eac4003da1e8
Sonali <sonali@cloudmatetechnologies.com>
2023-02-08 10:09:49 +0530

    add some changes

--- js/navbar.js
+++ js/navbar.js
@@ -4,57 +4,19 @@ function handleToggle() {
 }
 
 function displayData(id) {
-    document.getElementById(id).classList.toggle("display-none-for-responsive")
+  if(window.screen.width < 991)
+    document.getElementById(id).classList.toggle("footer-items-container")
 }
 
 window.onscroll = function() {handleCarouselPosition()};
 function handleCarouselPosition(){
     if (document.documentElement.scrollTop > 350) {
-        document.getElementById('offersCarousel').classList.add('offersCarousel')
+        document.getElementById('offersCarousel')?.classList.add('offersCarousel')
       }
       else{
-        document.getElementById('offersCarousel').classList.remove('offersCarousel')
+        document.getElementById('offersCarousel')?.classList.remove('offersCarousel')
       }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(function () {
-//     $("#carousel-multiple").on("slide.bs.carousel", function (e) {
-//       var itemsPerSlide = parseInt($(this).attr("data-maximum-items-per-slide")),
-//         totalItems = $(".carousel-item", this).length,
-//         reserve = 1, //do not change
-//         $itemsContainer = $(".carousel-inner", this),
-//         it = itemsPerSlide + reserve - (totalItems - e.to);
-  
-//       if (it > 0) {
-//         for (var i = 0; i < it; i++) {
-//           $(".carousel-item", this)
-//             .eq(e.direction == "left" ? i : 0)
-//             // append slides to the end/beginning
-//             .appendTo($itemsContainer);
-//         }
-//       }
-//     });
-//   });
   
 
 
